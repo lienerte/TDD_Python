034990ea4b0396d667b203a7bc47587f26351625
Lienert <Eric.Lienert@austin.utexas.edu>
2016-06-21 08:50:44 -0500

    Added more functionality testing: textBox of ToDo list, Input is saved, and that after one text box is used another pops up

--- TDD_Project/superlists/functional_tests.py
+++ TDD_Project/superlists/functional_tests.py
@@ -4,6 +4,7 @@ https://sites.google.com/a/chromium.org/chromedriver/getting-started
 """
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -19,7 +20,24 @@ class NewVisitorTest(unittest.TestCase):
     #Asked to enter a To-Do list; enter various items and save them
     def test_start_list_and_retrieve(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.browser.title)
+            #Header says To-Do
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+            #To-Do list ready to be added to
+        inputBox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputBox.get_attribute('placeholder'),
+            'Enter a to-do item',
+        )
+            #Input is added to the text box
+        inputBox.send_keys("Milk and cheese")
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Milk and cheese' for row in rows),
+        )
+            #Option to add to another text box
+            #Page updates and shows both on list
         self.fail('Finish the test')
 
 if __name__ == '__main__':
