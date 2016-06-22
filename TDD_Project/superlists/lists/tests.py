d5b8f6e8a86dc0711f8de7506222ef15d58bc3e2
Eric Lienert <lienerte@gmail.com>
2016-06-22 16:04:52 -0500

    Got through chapter 6. some problem with 404 error wont go away so reverting to previous commit

--- TDD_Project/superlists/lists/tests.py
+++ TDD_Project/superlists/lists/tests.py
@@ -19,11 +19,6 @@ class InitialTest(TestCase):
 		expected_html = render_to_string('home.html', request = request)
 		self.assertEqual(response.content.decode(), expected_html)
 
-	def test_home_page_only_saves_items_when_necessary(self):
-		request = HttpRequest()
-		home_page(request)
-		self.assertEqual(Item.objects.count(), 0)
-
 class ListAndItemModelsTest(TestCase):
 
 	def test_saving_and_retrieving_items(self):
@@ -75,6 +70,12 @@ class ListViewTest(TestCase):
 		response = self.client.get('/lists/%d/' % (list_.id,))
 		self.assertTemplateUsed(response, 'list.html')
 
+	def test_passes_correct_list_to_template(self):
+		correct_list = List.objects.create()
+		other_list = List.objects.create()
+		response = self.client.get('/lists/%d/' % (correct_list.id,))
+		self.assertEqual(response.context['list'], correct_list)
+
 class NewListTest(TestCase):
 
 	def test_redirects_after_POST(self):
@@ -94,3 +95,30 @@ class NewListTest(TestCase):
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, "A new list item")
+
+class NewItemTest(TestCase):
+
+	def test_can_save_POST_request_to_existing_list(self):
+		correct_list = List.objects.create()
+		other_list = List.objects.create()
+
+		self.client.post(
+			'/lists/%d/add_item' % (correct_list.id,),
+			data = {'item_text' : "A new item for an existing list"}
+		)
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, "A new item for an existing list")
+		self.assertEqual(new_item.list, correct_list)
+
+	def test_redirects_to_list_view(self):
+		correct_list = List.objects.create()
+		other_list = List.objects.create()
+
+		response = self.client.post(
+			'lists/%d/add_item' % (correct_list.id,),
+			data = {'item_text' : "A new item for an existing list"}
+		)
+
+		self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
