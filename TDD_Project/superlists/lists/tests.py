e12a726b592b8fa6e9778fcc3dd6f89943e9f58a
lienerte <lienerte@gmail.com>
2016-06-22 10:34:52 -0500

    Every list now has its own URL but now canot add multiple items to same list

--- TDD_Project/superlists/lists/tests.py
+++ TDD_Project/superlists/lists/tests.py
@@ -55,18 +55,24 @@ class ListAndItemModelsTest(TestCase):
 
 class ListViewTest(TestCase):
 
-	def test_displays_all_items(self):
-		list_ = List.objects.create()
-		Item.objects.create(text = "itemey 1", list = list_)
-		Item.objects.create(text = "itemey 2", list = list_)
+	def test_displays_only_items_for_that_list(self):
+		correct_list = List.objects.create()
+		Item.objects.create(text = "itemey 1", list = correct_list)
+		Item.objects.create(text = "itemey 2", list = correct_list)
+		other_list = List.objects.create()
+		Item.objects.create(text = "other item 1", list = other_list)
+		Item.objects.create(text = "other item 2", list = other_list)
 
-		response = self.client.get('/lists/the-only-list-in-the-world/')
+		response = self.client.get('/lists/%d/' % (correct_list.id,))
 
 		self.assertContains(response, "itemey 1")
 		self.assertContains(response, "itemey 2")
+		self.assertNotContains(response, "other item 1")
+		self.assertNotContains(response, "other item 2")
 
 	def test_uses_list_templates(self):
-		response = self.client.get('/lists/the-only-list-in-the-world/')
+		list_ = List.objects.create()
+		response = self.client.get('/lists/%d/' % (list_.id,))
 		self.assertTemplateUsed(response, 'list.html')
 
 class NewListTest(TestCase):
@@ -76,7 +82,8 @@ class NewListTest(TestCase):
 			'/lists/new',
 			data={'item_text': 'A new list item'}
 		)
-		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+		new_list = List.objects.first()
+		self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
 	def test_can_save_a_POST_request(self):
 		self.client.post(
