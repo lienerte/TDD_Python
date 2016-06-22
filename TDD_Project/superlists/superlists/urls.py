7d70c87f46fb1c486fcddaa23ac8620daaf0c507
Eric Lienert <lienerte@gmail.com>
2016-06-22 16:28:26 -0500

    Finished chapter 6.  Running with 0 errors.  Need to look at last commit diff to see what I did wrong.  I think it had to do with urls

--- TDD_Project/superlists/superlists/urls.py
+++ TDD_Project/superlists/superlists/urls.py
@@ -21,6 +21,6 @@ from lists import urls as list_urls
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
-	url(r'^$', list_views.home_page, name = 'home'),
+	url(r'^$', list_views.home_page, name='home'),
 	url(r'^lists/', include(list_urls)),
 ]
